a1cec26721d188eaf690c71b4efcd1d3d87e8bc3
alvinyanson <a.yanson@arcanys.com>
2024-08-15 14:48:25 +0800

    feat: added singleton real use case example

---
+++ Singleton/Practical/AppCounterManager/AppCounterManager.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Concurrent;
+
+namespace AppCounterManager
+{
+    public sealed class AppCounterManager
+    {
+        private static readonly Lazy<AppCounterManager> _instance = new Lazy<AppCounterManager>(() => new AppCounterManager());
+
+        private static int Counter = 0;
+
+        private ConcurrentDictionary<string, long> counters;
+
+
+        public AppCounterManager()
+        {
+            Counter++;
+            counters = new ConcurrentDictionary<string, long>();
+
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        public void IncrementCounter(string counterName)
+        {
+            counters.AddOrUpdate(counterName, 1, (key, currentValue) => currentValue + 1);
+        }
+
+        public long GetCounterValue(string counterName)
+        {
+            return counters.TryGetValue(counterName, out var value) ? value : 0;
+        }
+
+        public static AppCounterManager Instance => _instance.Value;
+
+    }
+}
---
+++ Singleton/Practical/AppCounterManager/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AppCounterManager
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Parallel.For(0, 10, i =>
+            {
+                AppCounterManager.Instance.IncrementCounter("UserLogins");
+            });
+
+
+            long currentLogins = AppCounterManager.Instance.GetCounterValue("UserLogins");
+         
+            
+            Console.WriteLine($"Current logins: {currentLogins}");
+        }
+    }
+}
